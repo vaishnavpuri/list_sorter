0f88a449cda5f53b60cb610b0f6b87b60f08b95f
Vaishnav Puri <56321564+vaishnavpuri@users.noreply.github.com>
2023-07-11 11:23:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import numpy as np  # Import NumPy for array conversion
 
 def get_names_from_csv(file):
     # Read the CSV file
@@ -12,15 +13,15 @@ def get_names_from_csv(file):
     else:
         names = []
         st.write("Column 'Telegram handle' not found.")
-    return names
+    return np.array(names)  # Convert list to NumPy array and return
 
 st.title('Telegram Handle Extractor')
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
     names = get_names_from_csv(uploaded_file)
-    if names:
-        st.write(names)  # Print the list to the Streamlit app
+    if names.size:  # Use .size for NumPy arrays instead of Python's len() function
+        st.write(names)  # Print the array to the Streamlit app
     else:
         st.write("No names found in the 'Telegram handle' column.")
 else:
