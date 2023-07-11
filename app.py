ad951ddc4d1c6d48bfdb220f8cb6a171ab0071e4
Vaishnav Puri <56321564+vaishnavpuri@users.noreply.github.com>
2023-07-11 11:26:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 import pandas as pd
 import streamlit as st
-import numpy as np  # Import NumPy for array conversion
+import numpy as np
 
 def get_names_from_csv(file):
-    # Read the CSV file
     df = pd.read_csv(file)
-
-    # Check if 'Telegram handle' column exists in the dataframe
     if 'Telegram handle' in df.columns:
-        # Get the column and convert to list
-        names = df['Telegram handle'].tolist()
+        names = df[df['Telegram handle'] != '-']['Telegram handle'].tolist()
     else:
         names = []
         st.write("Column 'Telegram handle' not found.")
-    return np.array(names)  # Convert list to NumPy array and return
+    return np.array(names)
 
 st.title('Telegram Handle Extractor')
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
     names = get_names_from_csv(uploaded_file)
-    if names.size:  # Use .size for NumPy arrays instead of Python's len() function
-        st.write(names)  # Print the array to the Streamlit app
+    if names.size: 
+        st.write(f'users: {np.array2string(names, separator=", ", formatter={"str_kind": lambda x: f"\"{x}\""})}') 
     else:
         st.write("No names found in the 'Telegram handle' column.")
 else:
